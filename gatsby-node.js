225267e4181705a96f3d80c626b8fa18a3a7ae5a
Andrew Davis <atd285@gmail.com>
2020-06-12 13:13:35 -0400

    gatsby-node setup

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,58 +1,127 @@
-const {
-  createFilePath,
-  sourceInstanceName,
-} = require("gatsby-source-filesystem");
-const path = require("path");
-exports.onCreateNode = async ({node, actions, getNode}) => {
+// You can delete this file if you're not using it
+/* eslint-disable no-case-declarations */
+/* eslint-disable no-undef */
+const path = require(`path`);
+const {createFilePath} = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({node, getNode, actions}) => {
   const {createNodeField} = actions;
-  // look for mdx files
-  if (node.internal.type === "Mdx") {
-    // do some stuff
-    const value = createFilePath({node, getNode});
-    console.log(value);
-    // value is /about and /blog-1
-    createNodeField({
-      name: "slug",
-      node,
-      value: `/new${value}`,
-    });
-    console.log(node);
-    const field = getNode(node.parent);
-    console.log({field});
-    // createNodeField({
-    //   name: "slug",
-    //   node,
-    //   value: `/${value}`,
-    // });
+
+  try {
+    if (node.internal.type === "Mdx") {
+      const {sourceInstanceName} = getNode(node.parent);
+
+      let slug = "";
+      switch (sourceInstanceName) {
+        case "projects":
+          slug = createFilePath({
+            node,
+            getNode,
+          });
+
+          createNodeField({
+            node,
+            name: "slug",
+            value: `/${sourceInstanceName}${slug}`,
+          });
+
+          createNodeField({
+            node,
+            name: "type",
+            value: "project",
+          });
+
+          const dirSplit = path.parse(slug).dir.split(path.sep);
+          if (dirSplit.length > 0 && dirSplit[0] === "") {
+            dirSplit.shift(); // because path starts with /, '' is always at position 0
+          }
+
+          if (dirSplit.length) {
+            createNodeField({
+              node,
+              name: "projectType",
+              value: dirSplit[0],
+            });
+          } else {
+            throw new Error("each project needs a type");
+          }
+          break;
+        case "posts":
+          slug = createFilePath({
+            node,
+            getNode,
+          });
+
+          createNodeField({
+            node,
+            name: "slug",
+            value: `/blog${slug}`,
+          });
+
+          createNodeField({
+            node,
+            name: "type",
+            value: "post",
+          });
+          break;
+        case "pages":
+        default:
+          return node;
+      }
+    }
+  } catch (e) {
+    throw new Error(e.message);
+    console.log({e});
   }
 };
 
-exports.createPages = async ({graphql, actions, reporter}) => {
+exports.createPages = ({graphql, actions}) => {
   const {createPage} = actions;
-  const result = await graphql(`
-    query {
-      allMdx {
-        edges {
-          node {
-            id
-            fields {
-              slug
+
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMdx {
+          edges {
+            node {
+              fields {
+                slug
+                type
+              }
             }
           }
         }
       }
-    }
-  `);
-  if (result.errors) {
-    reporter.panicOnBuild("🚨  ERROR: Loading 'createPages' query");
-  }
-  const posts = result.data.allMdx.edges;
-  posts.forEach(({node}, index) => {
-    console.log(node.fields);
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/post.tsx`),
-      context: {id: node.id},
+    `).then((result) => {
+      if (result.errors) {
+        console.error(result.errors);
+        reject(result.errors);
+      }
+
+      result.data.allMdx.edges.forEach(({node}) => {
+        const templatePath =
+          node.fields.type === "project"
+            ? "./src/templates/project.js"
+            : "./src/templates/post.js";
+
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(templatePath),
+          context: {
+            // Data passed to context is available in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        });
+      });
+      resolve();
     });
+  });
+};
+
+exports.onCreateWebpackConfig = ({actions}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
   });
 };
