d6a1b8c5237e81d4c63ab70aab5ba4605842c732
Andrew Davis <atd285@gmail.com>
2020-06-17 14:17:52 -0400

    landing page finished!

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -3,7 +3,27 @@ import {css} from '@emotion/core';
 import Img from 'gatsby-image';
 import {useStaticQuery, graphql} from 'gatsby';
 function LandingImage() {
-  return <div></div>;
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: {eq: "me.jpg"}) {
+        childImageSharp {
+          fixed(width: 300, height: 400) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Img
+      fixed={data.file.childImageSharp.fixed}
+      css={css`
+        margin-top: 50px;
+        border-radius: 20px;
+        filter: opacity(0.8);
+      `}
+    />
+  );
 }
 
 export default LandingImage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {graphql, Link} from 'gatsby';
+import {graphql} from 'gatsby';
+import {css} from '@emotion/core';
 import Layout from '../components/layout';
 import {StyledContainer} from '../shared/pattern';
-import Debug from '../components/Debug';
+import ProfileImage from '../components/ProfileImage';
 
 function IndexPage({data}: any): JSX.Element {
   // const title =
   //   data.allMdx.edges[0].node.frontmatter.path ?? "no title available";
   return (
     <Layout>
-      <Debug data={data} />
       <StyledContainer>
+        <ProfileImage />
         <h1>nice to meet you!</h1>
+        <div
+          css={css`
+            font-size: 1.3em;
+          `}
+        >
+          My name's Andrew Davis
+          <br />
+          I’m a creative, ambitious and enterprising web developer.
+        </div>
       </StyledContainer>
     </Layout>
   );
